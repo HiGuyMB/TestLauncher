5d05eff527c33637345f50eac20ba1f2d7396969
HiGuy Smith <higuymb@gmail.com>
2017-06-29 17:51:57 -0400

    Some basic UI

--- MBLauncher-Windows/MainWindow.xaml.cs
+++ MBLauncher-Windows/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MBLauncherLib;
+using MBLauncherLib.JsonTemplates;
 
 namespace MBLauncher_Windows
 {
@@ -20,9 +22,41 @@ namespace MBLauncher_Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        LauncherConfig config;
+
         public MainWindow()
         {
             InitializeComponent();
+            config = new LauncherConfig(new Uri("https://marbleblast.com/files/launcher/config.json"));
+
+            Task.Run(config.DownloadConfig).ContinueWith((Task<bool> task) =>
+            {
+                OnMainThread(() =>
+                {
+                    this.WaitingLabel.Text = "Loaded Config";
+                });
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.WaitingLabel.Text = "Installing MBP...";
+            ModConfig mod = config.mods["platinum"];
+            Task.Run(() =>
+            {
+                return mod.InstallMod("C:\\Users\\***REMOVED***\\Desktop\\MBP");
+            }).ContinueWith((Task<bool> task) =>
+            {
+                OnMainThread(() =>
+                {
+                    this.WaitingLabel.Text = "Installed";
+                });
+            });
+        }
+
+        private void OnMainThread(Action action)
+        {
+            App.Current.Dispatcher.Invoke(action);
         }
     }
 }
