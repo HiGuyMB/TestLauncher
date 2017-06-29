e1b99ac1ccf74197f8ec35212e565384a352fffc
HiGuy Smith <higuymb@gmail.com>
2017-06-29 18:16:26 -0400

    Mac window base

--- MBLauncher-Mac/MainWindow.cs
+++ MBLauncher-Mac/MainWindow.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using Foundation;
 using AppKit;
+using MBLauncherLib.JsonTemplates;
 
 namespace MBLauncherMac
 {
     public partial class MainWindow : NSWindow
     {
+		LauncherConfig config;
+		
         public MainWindow(IntPtr handle) : base(handle)
         {
         }
@@ -19,6 +23,37 @@ namespace MBLauncherMac
         public override void AwakeFromNib()
         {
             base.AwakeFromNib();
+			config = new LauncherConfig(new Uri("https://marbleblast.com/files/launcher/config.json"));
+
+			Task.Run(config.DownloadConfig).ContinueWith((Task<bool> task) =>
+			{
+				OnMainThread(() =>
+				{
+                    this.WaitingLabel.StringValue = "Loaded Config";
+				});
+			});
+
+		}
+        
+        partial void Install(Foundation.NSObject sender)
+        {
+            this.WaitingLabel.StringValue = "Installing MBP...";
+            ModConfig mod = config.mods["platinum"];
+            Task.Run(() =>
+            {
+                return mod.InstallMod("TODO: Path Manager");
+            }).ContinueWith((Task<bool> task) =>
+            {
+                OnMainThread(() =>
+                {
+                    this.WaitingLabel.StringValue = "Installed";
+                });
+            });
+        }
+
+        void OnMainThread(Action action)
+        {
+            InvokeOnMainThread(action);
         }
     }
 }
--- MBLauncher-Mac/MainWindow.designer.cs
+++ MBLauncher-Mac/MainWindow.designer.cs
@@ -1,7 +1,29 @@
-﻿namespace MBLauncherMac
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace MBLauncherMac
 {
-    [global::Foundation.Register("MainWindow")]
-    public partial class MainWindow
+    [Register ("MainWindow")]
+    partial class MainWindow
     {
+        [Outlet]
+        AppKit.NSTextField WaitingLabel { get; set; }
+
+        [Action ("Install:")]
+        partial void Install (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
+        {
+            if (WaitingLabel != null) {
+                WaitingLabel.Dispose ();
+                WaitingLabel = null;
+            }
+        }
     }
 }
